4929d2426ebe4732a0bceb36a2aa9f7f525bd335
colorrin <mb.andres@gmail.com>
2026-04-21 19:28:54 +0200

    RGB Dry/Wet

--- src/editor/settings/shaders/pixelate_shader_settings.ts
+++ src/editor/settings/shaders/pixelate_shader_settings.ts
@@ -7,7 +7,59 @@ export const PIXELATE_SHADER_SETTINGS: ISetting[] = [
     field: "pixelSize",
     type: "integer",
     onchange: (id: number, value: string | number | boolean, owner: string) => {
-      executeCommand(new SetShaderFieldCommand(id, "pixelSize", value, owner));
+      executeCommand(
+        new SetShaderFieldCommand(
+          id,
+          "pixelSize",
+          parseInt(value.toString()),
+          owner,
+        ),
+      );
+    },
+    signalizable: true,
+  },
+  {
+    field: "redDryWet",
+    type: "float",
+    onchange: (id: number, value: string | number | boolean, owner: string) => {
+      executeCommand(
+        new SetShaderFieldCommand(
+          id,
+          "redDryWet",
+          parseFloat(value.toString()),
+          owner,
+        ),
+      );
+    },
+    signalizable: true,
+  },
+  {
+    field: "greenDryWet",
+    type: "float",
+    onchange: (id: number, value: string | number | boolean, owner: string) => {
+      executeCommand(
+        new SetShaderFieldCommand(
+          id,
+          "greenDryWet",
+          parseFloat(value.toString()),
+          owner,
+        ),
+      );
+    },
+    signalizable: true,
+  },
+  {
+    field: "blueDryWet",
+    type: "float",
+    onchange: (id: number, value: string | number | boolean, owner: string) => {
+      executeCommand(
+        new SetShaderFieldCommand(
+          id,
+          "blueDryWet",
+          parseFloat(value.toString()),
+          owner,
+        ),
+      );
     },
     signalizable: true,
   },
