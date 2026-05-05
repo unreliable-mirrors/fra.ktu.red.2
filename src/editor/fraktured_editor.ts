f4e5e01e0b3d12c8a62ec5bf9bc29dcc19b6593a
colorrin <mb.andres@gmail.com>
2026-05-05 21:49:50 +0200

    Paste support

--- src/editor/fraktured_editor.ts
+++ src/editor/fraktured_editor.ts
@@ -2,11 +2,14 @@ import {
   DataStore,
   SceneState,
   RedViewerComponent,
+  DisplayLayerState,
 } from "fra.ktu.red-component";
 import { EditorUIComponent } from "./ui/editor_ui";
 import { executeCommand } from "../ktu/helpers/commands_manager";
 import { NewStateCommand } from "./commands/new_state_command";
 import { EventDispatcher } from "fra.ktu.red-component";
+import { AddLayerCommand } from "./commands/layers/add_layer_command";
+import { LoadFileCommand } from "./commands/load_file_command";
 
 export class FrakturedEditor {
   canvasContainer: HTMLElement;
@@ -55,5 +58,36 @@ export class FrakturedEditor {
         DataStore.getInstance().touchIds("editorScene");
       },
     );
+
+    document.addEventListener("paste", async (e: ClipboardEvent) => {
+      if (e.clipboardData) {
+        for (const file of e.clipboardData.files) {
+          if (
+            file.type.indexOf("image/") === 0 ||
+            file.type.indexOf("video/") === 0
+          ) {
+            const fr: FileReader = new FileReader();
+            fr.onload = (e) => {
+              const payload: string = e.target!.result as string;
+              console.log("PASTE", payload);
+              executeCommand(new AddLayerCommand("video"));
+              const layers = DataStore.getInstance().getStore(
+                "editorScene.layers",
+              ) as DisplayLayerState[];
+              const state = layers[layers.length - 1];
+              executeCommand(
+                new LoadFileCommand("editorScene", state.id, payload),
+              );
+            };
+            if (file.size < 104857600) {
+              fr.readAsDataURL(file);
+            } else {
+              //TODO: Implement an alert system for this
+              console.log("ERROR - No files larger than 100mb");
+            }
+          }
+        }
+      }
+    });
   }
 }
